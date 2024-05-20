3e1c1ed99f9312f7ac6dfb7b3ea9287e2774774a
lucasbizachi <bizachilucas@gmail.com>
2024-05-20 14:52:42 -0300

    habilitando botão de logar

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,19 @@
+import React from "react";
 import { ButtonContainer } from "./styles";
-import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+export interface IButtonProps {
+  title: string;
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
+const Button: React.FC<IButtonProps> = ({ title, onClick, disabled }) => {
+  return (
+    <ButtonContainer onClick={onClick} disabled={disabled}>
+      {title}
+    </ButtonContainer>
+  );
 };
 
 export default Button;
+
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
 
+interface ButtonContainerProps {
+  onClick?: () => void;
+  disabled?: boolean;
+}
 
-export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100%;
+  height: 42px;
+  background-color: ${(props) => (props.disabled ? '#CCCCCC' : '#81259D')}; /* Altera a cor de fundo se o botão estiver desabilitado */
+  color: #FFF;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
+  border: 1px solid #81259D;
+  border-radius: 21px;
 
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  &:hover {
+    opacity: 0.6;
+    cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')}; /* Altera o cursor se o botão estiver desabilitado */
+  }
+`;
